58dddd3edc8ca534d47167011e768422118e850d
Raissa <raissa.feitosa06@gmail.com>
2026-06-24 10:35:40 -0300

    feat: criação dos componentes Navbar.tsx e Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
-
-  return (
-    <>
-      <Home/>
-    </>
-  )
+	return (
+		<>
+			<Navbar />
+			<Home />
+			<Footer />
+		</>
+	)
 }
 
 export default App
