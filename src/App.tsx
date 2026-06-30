a9406aec9e21536ac3f84cdcf9d9c066fc22634b
Raissa <raissa.feitosa06@gmail.com>
2026-06-30 12:42:10 -0300

    feat: criação dos componentes CardTema e ListaTemas, com suas respectivas lógicas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import ListaTemas from './components/tema/listatemas/ListaTemas'
 import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
@@ -17,6 +18,7 @@ function App() {
 							<Route path="/" element={<Login />} />
 							<Route path="/home" element={<Home />} />
 							<Route path="/cadastro"	element={<Cadastro />}/>
+							<Route path="/temas" element={<ListaTemas />} />
 						</Routes>
 					</div>
 					<Footer />
