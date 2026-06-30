e3b30120127538e9e4d21e480e51babc9535b759
Raissa <raissa.feitosa06@gmail.com>
2026-06-30 15:51:23 -0300

    feat: criação do componente DeletarTema, incluindo sua lógica

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import ListaTemas from './components/tema/listatemas/ListaTemas'
 import FormTema from './components/tema/formtema/FormTema'
+import DeletarTema from './components/tema/deletartema/DeletarTema'
 import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
@@ -22,6 +23,7 @@ function App() {
 							<Route path="/temas" element={<ListaTemas />} />
 							<Route path="/cadastrartema" element={<FormTema />} />
 							<Route path="/editartema/:id" element={<FormTema />} />
+							<Route path="/deletartema/:id" element={<DeletarTema />} />
 						</Routes>
 					</div>
 					<Footer />
